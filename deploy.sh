5a262bd476f3e9a28180b39679f69d9349dcd5b6
boxboy523 <rlawnsdud523@gmail.com>
2023-10-19 23:31:49 +0900

    rebuilding site Thu Oct 19 23:31:49 KST 2023

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,20 @@ git push origin master
 # Come Back up to the Project Root
 cd ..
 
+cd ./theme/risotto
+
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+
+git push origin main
+
+cd ../..
 
 # blog 저장소 Commit & Push
 git add .
